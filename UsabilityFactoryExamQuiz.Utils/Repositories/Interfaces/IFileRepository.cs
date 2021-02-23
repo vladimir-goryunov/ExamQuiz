4c4beafbac956b74fa1270318ab9d5e65b101e43
Vladimir Goryunov <vladimir.goryunov@constant.obninsk.ru>
2021-02-23 19:22:54 +0300

    Code refactoring

--- UsabilityFactoryExamQuiz.Utils/Repositories/Interfaces/IFileRepository.cs
+++ UsabilityFactoryExamQuiz.Utils/Repositories/Interfaces/IFileRepository.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using UsabilityFactoryExamQuiz.Model.DataContract;
 
 namespace UsabilityFactoryExamQuiz.Utils.Repositories.Interfaces
 {
     public interface IFileRepository
     {
-        public void SaveFiles(IEnumerable<IFormFile> files);
+        void SaveFile(string fileName, IFormFile fileContent);
     }
 }
